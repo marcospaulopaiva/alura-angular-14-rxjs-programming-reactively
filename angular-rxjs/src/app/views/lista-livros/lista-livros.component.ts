ddfb39d1c1631aee8df9b561c3196cfe4ec31600
Marcos Paulo <marcos.poo@gmail.com>
2024-07-02 22:21:44 -0300

    Aula 5 - Otimizando a busca com RxJS
    Nesta aula:
    - Desenvolver uma busca dinâmica;
    - Utilizar o pipe async;
    - Encadear os operadores switchMap, filter e debounceTime para otimizar a performance da aplicação.

--- angular-rxjs/src/app/views/lista-livros/lista-livros.component.ts
+++ angular-rxjs/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,33 +1,33 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Item, Livro } from 'src/app/models/interfaces';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { switchMap, map, tap, filter, debounceTime, distinctUntilChanged } from 'rxjs';
+import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy {
+export class ListaLivrosComponent {
 
-  listaLivros: Livro[];
-  campoBusca: string = ''
-  subscription: Subscription
-  livro: Livro
+  campoBusca = new FormControl()
 
   constructor(private service: LivroService) { }
 
-
-  bucsarLivros(){
-    this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: (items) => {
-        this.listaLivros = this.livrosResultadoParaLivros(items)
-      },
-      error: erro => console.error(erro),
-
-    })
-  }
+  livrosEncontrados$ = this.campoBusca.valueChanges
+    .pipe(
+      debounceTime(PAUSA),
+      filter((valorDigitado) => valorDigitado.length >= 3),
+      tap(() => console.log('Fluxo inicial')),
+      distinctUntilChanged(),
+      switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+      tap((retornoAPI) => console.log(retornoAPI)),
+      map((items) => this.livrosResultadoParaLivros(items))
+    )
 
 livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
   return items.map(item => {
@@ -35,11 +35,4 @@ livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
   })
 }
 
-
-  ngOnDestroy(){
-    this.subscription.unsubscribe()
-  }
 }
-
-
-
