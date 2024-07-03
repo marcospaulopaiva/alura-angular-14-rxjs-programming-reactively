93611eea752b80760d417c84a9ef5cefeb0049a5
Marcos Paulo <marcos.poo@gmail.com>
2024-07-02 22:59:13 -0300

    Aula 6 - Tratamento de erros
    Nesta aula:
    - Capturar erros com os operadores catchError e throwError;
    - Utilizar o operador EMPTY;
    - Mostrar mensagens de erro para a pessoa usuária.

--- angular-rxjs/src/app/views/lista-livros/lista-livros.component.ts
+++ angular-rxjs/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,11 +1,11 @@
+import { switchMap, map, tap, filter, debounceTime, throwError, catchError, EMPTY, of } from 'rxjs';
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { switchMap, map, tap, filter, debounceTime, distinctUntilChanged } from 'rxjs';
-import { Item } from 'src/app/models/interfaces';
-import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { FormControl } from '@angular/forms';
 
-const PAUSA = 300
+const PAUSA = 300;
 
 @Component({
   selector: 'app-lista-livros',
@@ -14,25 +14,47 @@ const PAUSA = 300
 })
 export class ListaLivrosComponent {
 
-  campoBusca = new FormControl()
+  campoBusca = new FormControl();
+  mensagemErro = ''
+  livrosResultado: LivrosResultado;
 
   constructor(private service: LivroService) { }
+
+  // totalDeLivros$ = this.campoBusca.valueChanges
+  // .pipe(
+  //   debounceTime(PAUSA),
+  //   filter((valorDigitado) => valorDigitado.length >= 3),
+  //   tap(() => console.log('Fluxo inicial')),
+  //   switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+  //   map(resultado => this.livrosResultado = resultado),
+  //   catchError(erro => {
+  //     console.log(erro)
+  //     return of()
+  //   })
+  // )
 
   livrosEncontrados$ = this.campoBusca.valueChanges
     .pipe(
       debounceTime(PAUSA),
       filter((valorDigitado) => valorDigitado.length >= 3),
       tap(() => console.log('Fluxo inicial')),
-      distinctUntilChanged(),
       switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+      map(resultado => this.livrosResultado = resultado),
       tap((retornoAPI) => console.log(retornoAPI)),
-      map((items) => this.livrosResultadoParaLivros(items))
+      map(resultado => resultado.items ?? []),
+      map((items) => this.livrosResultadoParaLivros(items)),
+      catchError((erro) => {
+        // this.mensagemErro ='Ops, ocorreu um erro. Recarregue a aplicação!'
+        // return EMPTY
+        console.log(erro)
+        return throwError(() => new Error(this.mensagemErro ='Ops, ocorreu um erro. Recarregue a aplicação!'))
+      })
     )
 
-livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
-  return items.map(item => {
-    return new LivroVolumeInfo(item)
-  })
-}
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
+  }
 
 }
